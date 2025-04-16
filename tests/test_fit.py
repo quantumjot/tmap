fe1abf1908a9a9c9833678ea73df14e33098c6bf
quantumjot <code@arlowe.co.uk>
2025-04-16 11:40:00 -0400

    update CI, tests

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -2,28 +2,39 @@ import numpy as np
 import pytest
 
 from tmap import InitialLayout, TemporalMAP
-
-TEST_DATA = [
-    np.random.random((15, 3)),
-    np.random.random((10, 3)),
-]
+from tmap.simulate import simulate_trajectories
 
 
-def test_map():
+@pytest.fixture
+def sim_data():
+    TEST_DATA = [
+        np.random.random((15, 3)),
+        np.random.random((10, 3)),
+    ]
+    return TEST_DATA
+
+
+# @pytest.fixture
+# def sim_data():
+#     TEST_DATA = simulate_trajectories()
+#     return TEST_DATA
+
+
+def test_map(sim_data):
     t = TemporalMAP()
-    y = t.fit(TEST_DATA)
+    y = t.fit(sim_data)
     assert y.ndim == 2
 
 
-@pytest.mark.parametrize("layout", (l for l in InitialLayout))
-def test_layout(layout):
+@pytest.mark.parametrize("layout", (layout for layout in InitialLayout))
+def test_layout(sim_data, layout):
     t = TemporalMAP(layout=layout)
-    y = t.fit(TEST_DATA)
+    y = t.fit(sim_data)
     assert y.ndim == 2
 
 
 @pytest.mark.parametrize("ndim", (2, 3))
-def test_ndim(ndim):
+def test_ndim(sim_data, ndim):
     t = TemporalMAP(n_components=ndim)
-    y = t.fit(TEST_DATA)
+    y = t.fit(sim_data)
     assert y.shape[-1] == ndim
